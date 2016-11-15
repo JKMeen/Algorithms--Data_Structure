6b3e3c689cfb9f28e896eaa404f10272e3a94cb1
JKMeen <rhkdals1206@naver.com>
2016-11-16 02:21:34 +0900

    update

--- C_Code/16-11-16-LineEditer2(input_int).cpp
+++ C_Code/16-11-16-LineEditer2(input_int).cpp
@@ -1,5 +1,3 @@
-//연결리스트 pDays={월,화,수,목,금, 토, 일}
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
@@ -16,20 +14,41 @@ typedef struct DlistNode {
 void printList(DlistNode* p)
 { // p는 headnode (pDays)
 	DlistNode *tmp = p->next;
-	printf("\n연결리스트 출력 >>> ");
+	printf("\n< 연결리스트 출력 > \n");
 	while (tmp != p) {
-		printf("< %s >", tmp->day);
+		printf("%d >> %s \n", tmp->line, tmp->day);
 		tmp = tmp->next;
 	}
 	printf("\n------------------------------------\n\n");
 }
 
+void inputsearch(DlistNode *head, DlistNode *newnode, int num)
+{
+	DlistNode *p = newnode->next;
+	while (p != head){
+		if (num == 0){
+			p->line += 1;
+		}
+		else if (num == 1){
+			p->line -= 1;
+		}
+		p = p->next;
+	}
+}
+
 // before : 선행 노드
 // new_node : 삽입될 노드 
-void dinsert_node(DlistNode *before, DlistNode *new_node)
+void dinsert_node(DlistNode *L, DlistNode *before, DlistNode *new_node)
 {
 	new_node->prev = before;
 	new_node->next = before->next;
+	if (before == L){
+		new_node->line = before->line;
+	}
+	else {
+		new_node->line = before->line + 1;
+	}
+	inputsearch(L, new_node, 0);
 	before->next->prev = new_node;
 	before->next = new_node;
 }
@@ -41,18 +60,33 @@ void dremove_node(DlistNode *phead_node, DlistNode *removed)
 	if (removed == phead_node) return;
 	removed->prev->next = removed->next;
 	removed->next->prev = removed->prev;
+	removed->line = NULL;
+
+	inputsearch(phead_node, removed, 1);
+
 	free(removed);
 }
 
-DlistNode *search(DlistNode *head, char x[])
+DlistNode *search(DlistNode *head, int instline)
 {
 	DlistNode *p = head->next;
 	while (p != head){
-		if (strcmp(p->day, x) == 0) return p;  // 탐색 성공
+		if (p->line == instline) return p;  // 탐색 성공
 		p = p->next;
 	}
 	return p;  // 탐색 실패일 경우 NULL 반환
 }
+
+/*
+DlistNode *search(DlistNode *head, char x[])
+{
+DlistNode *p = head->next;
+while (p != head){
+if (strcmp(p->day, x) == 0) return p;  // 탐색 성공
+p = p->next;
+}
+return p;  // 탐색 실패일 경우 NULL 반환
+}*/
 
 void main()
 {
@@ -61,8 +95,10 @@ void main()
 	char select;
 	char input[200];
 	char where[200];
+	int instline;
 
 	pDays = (DlistNode*)malloc(sizeof(DlistNode));
+	pDays->line = 0;
 	pDays->next = pDays;
 	pDays->prev = pDays;
 
@@ -70,11 +106,11 @@ void main()
 		printf("'a' : Add \n");
 		printf("'r' : Remove \n");
 		printf("'e' : Exit \n");
-		printf("Input Select >>> ");
+		printf("Select Input >>> ");
 		scanf("%c", &select);
 		fflush(stdin);
 		switch (select){
-		case 'a': 
+		case 'a':
 			new_node = (DlistNode *)malloc(sizeof(DlistNode));
 			printf("Input String >>> ");
 			fgets(input, sizeof(input), stdin);
@@ -85,24 +121,31 @@ void main()
 			new_node->prev = NULL;
 
 			if (pDays->next == pDays){
-				dinsert_node(pDays, new_node);
-			} else {
-				printf("Input where? (Input String) >>> ");
-				fgets(where, sizeof(where), stdin);
-				where[strlen(where) - 1] = '\0';
-				//scanf("%s", &input);
-				before = search(pDays, where);
-				dinsert_node(before, new_node);
+				dinsert_node(pDays, pDays, new_node);
+			}
+			else {
+				printf("where? >>> ");
+				scanf("%d", &instline);
+				before = search(pDays, instline);
+				dinsert_node(pDays, before, new_node);
+				/*printf("where? >>> ");
+				scanf("%d", &instline);
+				before = search(pDays, instline);*/
+				//fgets(where, sizeof(where), stdin);
+				//where[strlen(where) - 1] = '\0';
+				//before = search(pDays, input);
 			}
 			printList(pDays);
 			break;
 
-		case 'r': 
-			printf("Remove what? (Input String) >>> ");
-			fgets(input, sizeof(input), stdin);
-			input[strlen(input) - 1] = '\0';
+		case 'r':
+			printf("where? >>> ");
+			scanf("%d", &instline);
+			remove = search(pDays, instline);
+			//fgets(input, sizeof(input), stdin);
+			//input[strlen(input) - 1] = '\0';
 			//scanf("%s", &input);
-			remove = search(pDays, input);
+			//remove = search(pDays, input);
 			if (remove == NULL){
 				break;
 			}
